dd23a92a1884e24c01d79b0639e9fa3a365b3676
Лёха домашний <macarena7777777@gmail.com>
2024-10-26 16:20:58 +0300

    created picture

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -3,11 +3,13 @@ import Navbar from '../../components/Navbar/Navbar'
 import MySelect from '../../components/Select/MySelect'
 import MyInput from '../../components/Input/MyInput'
 import styles from "./styles.module.css"
+import logo from "../../public/RICKANDMORTY.svg"
 
 function Characters() {
     return (
         <>
             <Navbar />
+            <img src={logo} alt="RICKANDMORTY" />
             <div className={styles.sorting}>
                 <MyInput />
                 <MySelect />
