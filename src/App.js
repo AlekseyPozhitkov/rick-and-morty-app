a8849818dc3ebed0e4ea24bad42ae822793376a8
Лёха домашний <macarena7777777@gmail.com>
2024-10-26 18:32:23 +0300

    created picture2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Characters from './pages/Characters/Characters'
+import Locations from './pages/Locations/Locations'
 
 function App() {
   return (
     <div>
       <Characters />
+      <Locations />
     </div>
   )
 }
--- src/pages/Locations/Locations.jsx
+++ src/pages/Locations/Locations.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo from "../../public/rick-and-morty-circle.svg"
 
 function Locations() {
     return (
-        <div>
-
-        </div>
+        <>
+            <img src={logo} alt="rick-and-morty-circle" />
+        </>
     )
 }
 
