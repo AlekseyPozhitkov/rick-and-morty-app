3e5c2a737e1c07bedec74a9ba4b38a8a35162715
Лёха домашний <macarena7777777@gmail.com>
2024-10-26 16:07:04 +0300

    created sort fields

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import MySelect from '../../components/Select/MySelect'
+import MyInput from '../../components/Input/MyInput'
+import styles from "./styles.module.css"
 
 function Characters() {
     return (
-        <div>
+        <>
             <Navbar />
-        </div>
+            <div className={styles.sorting}>
+                <MyInput />
+                <MySelect />
+                <MySelect />
+                <MySelect />
+            </div>
+        </>
     )
 }
 
